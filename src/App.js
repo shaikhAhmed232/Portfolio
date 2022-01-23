5f3100165d9d5f4a0a9858e77b94d6a8420f2adb
ShaikhAhmed232 <stitans270@gmail.com>
2022-01-23 12:34:15 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Hello world</h2>
+      <div className="cotent"></div>
+    </>
+  );
+}
+
+export default App;
