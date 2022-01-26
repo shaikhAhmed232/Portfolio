f000251296f7bb248c1a9ffa7fe04d02a21f7184
ShaikhAhmed232 <stitans270@gmail.com>
2022-01-26 19:57:57 +0530

    portfolio complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  const [navbar, setNavbar] = useState(false);
-
-  const changeNavBack = () => {
-    if (window.scrollY >= 80) {
-      setNavbar(true);
-    }
-  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
       <div className="container nav-container">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="/">
           SA Portfolio
-        </a>
+        </Link>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler collapsed"
           type="button"
           data-toggle="collapse"
           data-target="#navbarSupportedContent"
@@ -33,25 +27,45 @@ const Navbar = () => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav">
-            <li className="nav-item mx-2 active">
-              <a className="nav-link" href="#">
+            <li className="nav-item mx-2">
+              <NavLink
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                className="nav-link"
+                to="/"
+              >
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item mx-2">
-              <a className="nav-link" href="#">
+              <NavLink
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                className="nav-link"
+                to="/about"
+              >
                 About
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item mx-2">
-              <a className="nav-link" href="#">
+              <NavLink
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                className="nav-link"
+                to="/skills"
+              >
                 Skills
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item mx-2">
-              <a className="nav-link" href="#">
+              <NavLink
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                className="nav-link"
+                to="/contact"
+              >
                 Contact
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
